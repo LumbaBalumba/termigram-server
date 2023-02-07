d5cd4a945d9dfe5fdd13f84c5b91d549d3c37b62
i3alumba <vlad.lazar.ik528@yandex.ru>
2023-02-07 22:44:20 +0300

    Add CRUDs for user

---
+++ dto/user.ts
@@ -0,0 +1,10 @@
+export type UserSignUpDTO = {
+    name: string,
+    second_name: string,
+    login: string,
+    password: string
+}
+
+export type UserSignInDTO = {
+    login: string
+}
---
+++ services/user.ts
@@ -0,0 +1,21 @@
+import prisma from "./prisma-service"
+import {UserSignUpDTO, UserSignInDTO} from "../dto/user"
+
+export async function signUp({name, second_name, password, login}: UserSignUpDTO) {
+    return prisma.user.create({
+        data: {
+            name: name,
+            second_name: second_name,
+            login: login,
+            password: password
+        }
+    })
+}
+
+export async function signIn({login}: UserSignInDTO) {
+    return prisma.user.findUniqueOrThrow({
+        where: {
+            login: login
+        }
+    })
+}
